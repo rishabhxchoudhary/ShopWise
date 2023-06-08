3100a19f417d40cc80d0aae72b80a1c5a17a7073
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2023-06-08 22:22:44 +0530

    fix.

--- client/src/database/controller/userController.ts
+++ client/src/database/controller/userController.ts
@@ -1,23 +1,23 @@
-import { NextRequest, NextResponse } from 'next/server';
-import User, { IUser } from '../models/user';
+// import { NextRequest, NextResponse } from 'next/server';
+// import User, { IUser } from '../models/user';
 
-// Example of creating a new user
-const createUser = async (req: NextRequest, res: NextResponse): Promise<NextResponse> => {
-  try {
-    const { name, email, password } = req.body;
+// // Example of creating a new user
+// const createUser = async (req: NextRequest, res: NextResponse): Promise<NextResponse> => {
+//   try {
+//     const { name, email, password } = req.body;
 
-    const newUser: IUser = new User({
-      name,
-      email,
-      password,
-    });
+//     const newUser: IUser = new User({
+//       name,
+//       email,
+//       password,
+//     });
 
-    await newUser.save();
-    return res.json(newUser);
+//     await newUser.save();
+//     return res.json(newUser);
 
-  } catch (error) {
-    return res.json({error});
-  }
-};
+//   } catch (error) {
+//     return res.json({error});
+//   }
+// };
 
-export default createUser;
+// export default createUser;
