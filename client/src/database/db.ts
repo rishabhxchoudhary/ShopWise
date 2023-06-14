23903c1b8fabe9122d5e0f1542a56b1fefdd9bef
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2023-06-14 12:34:55 +0530

    try 3

--- client/src/database/db.ts
+++ client/src/database/db.ts
@@ -1,7 +1,5 @@
 // This file sets up a reusable connection to the MongoDB database using Mongoose.
 import mongoose, { Connection } from "mongoose";
-import Product from "./models/product";
-// import Product from "./models/Product";
 
 const MONGODB_URI = "mongodb+srv://root:root@cluster0.smibz3t.mongodb.net/";
 
@@ -39,5 +37,101 @@ async function connectToDatabase(): Promise<Connection> {
 
 // const db = connectToDatabase();
 // Product.find();
-export const product = Product;
 export default connectToDatabase;
+
+import { Document, Schema, Model, model, models } from "mongoose";
+interface ProductVariant {
+  option: string;
+  values: string[];
+  availability: string[];
+}
+
+interface ProductImages {
+  [key: string]: string;
+}
+
+interface ProductRating {
+  average: number;
+  count: number;
+}
+
+interface ProductReview {
+  author: string;
+  rating: number;
+  comment: string;
+}
+
+interface ProductSpecifications {
+  [key: string]: string[];
+}
+
+interface IProduct extends Document {
+  name: string;
+  description: string;
+  category: string;
+  brand: string;
+  price: number;
+  variants: ProductVariant[];
+  images: ProductImages;
+  ratings: ProductRating;
+  reviews: ProductReview[];
+  specifications: ProductSpecifications;
+  tags: string[];
+  metaDescription: string;
+}
+
+const productSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  variants: {
+    type: [Object],
+    required: true,
+  },
+  images: {
+    type: Object,
+    required: true,
+  },
+  ratings: {
+    type: Object,
+    default: {},
+  },
+  reviews: {
+    type: [Object],
+    default: [],
+  },
+  specifications: {
+    type: Object,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+  metaDescription: {
+    type: String,
+    default: "",
+  },
+});
+
+export const product =
+  (models.Product as Model<IProduct>) ||
+  model<IProduct>("Product", productSchema);
