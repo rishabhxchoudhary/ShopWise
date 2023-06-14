0f4f1f05583b69a2db91b99a98d475cf6d3033f9
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2023-06-14 14:30:05 +0530

    try 2

--- client/src/database/controller/productController.ts
+++ client/src/database/controller/productController.ts
@@ -1,4 +1,4 @@
-import Product from "../models/product";
+import Product from "@/database/models/product";
 
 async function getProductById(id: string) {
   const data = await Product.findOne({ _id: id });
